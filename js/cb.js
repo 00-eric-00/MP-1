ac504cebaaf4fdb1931ace72f956fd7cc8946dd1
luso00 <lusoclement01@gmail.com>
2023-02-28 17:44:47 +0800

    updating javascript

--- js/cb.js
+++ js/cb.js
@@ -1,10 +1,1 @@
-function radio() {
-  receptacle = document.getElementById("receptacle");
-  outlet = document.getElementById("outlet");
-  resistiveload = document.getElementById("resistiveload");
-  if (resistiveload.checked) {
-    receptacle.checked;
-  } else {
-    document.getElementsByName(resistive).unchecked;
-  }
-}
+const load_description = [];
